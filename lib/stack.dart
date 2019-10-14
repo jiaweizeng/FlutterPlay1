58104e5851ac00c57d88ed5a39bf82b6c80db9b9
曾嘉伟 <jiawei.zeng@inin88.com>
2019-10-14 18:11:55 +0800

    层叠

---
+++ lib/stack.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    var stack = new Stack(
+      alignment: const FractionalOffset(0.5, 0.2),
+      children: <Widget>[
+        new CircleAvatar(
+          backgroundImage: new NetworkImage(
+              'http://photocdn.sohu.com/20130416/Img372885486.jpg'),
+          radius: 100,
+        ),
+        new Container(
+          decoration: new BoxDecoration(color: Colors.lightBlue),
+          padding: EdgeInsets.all(5),
+          child: new Text('iiiiiiiiiaaaa'),
+        )
+      ],
+    );
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+      ),
+      home: Scaffold(
+        appBar: new AppBar(
+          title: new Text('ceng die '),
+        ),
+        body: Center(
+          child: stack,
+        ),
+      ),
+    );
+  }
+}
